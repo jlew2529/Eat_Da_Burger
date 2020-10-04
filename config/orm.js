553dc923c48f8a10d65f2d4ab0d6784d086b5a02
Jalen Lewis <jalenlew1995@gmail.com>
2020-10-03 22:19:16 -0500

    Modified the config files

--- config/orm.js
+++ config/orm.js
@@ -1,1 +1,51 @@
 var connection = require("../config/connection");
+
+var orm = {
+    selectAll: function(table, cb) {
+        var dbQuery = "SELECT * FROM " + table + ";";
+
+        connection.query(dbQuery, function(err,res){
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        })
+    }
+
+    insertOne: function(table, cols, vals, cb) {
+        var dbQuery = 
+            "INSERT INTO " + 
+            table + 
+            " (" + 
+            cols.toString() + 
+            ") " + "VALUES (" + 
+            createQmarks(vals.length) + 
+            ") ";
+
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err,res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });
+    }
+
+    updateOne: function(table, objColVals, condition, cb) {
+        var dbQuery = 
+            "UPDATE " + 
+            table + 
+            " SET " + 
+            translateSql(objColVals) + 
+            " WHERE " + 
+            condition;
+
+        console.log(dbQuery);
+        connection.query(dbQuery, vals, function(err,res) {
+            if (err) {
+                throw err;
+            }
+            cb(res);
+        });   
+    }
+}
