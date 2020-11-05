d1f3593e4b5982e49536b5cb5013a3bb439ad08e
Jalen Lewis <jalenlew1995@gmail.com>
2020-11-05 14:07:22 -0600

    burger.js edits

--- models/burger.js
+++ models/burger.js
@@ -1,9 +1,25 @@
+const { createOne } = require("../config/orm");
 var orm = require("../config/orm");
 
 var burger = {
-    selectAll: function (cb) {
+    selectAll: function(cb) {
         orm.selectAll("burgers", function (res) {
             cb(res);
         });
+    },
+    createOne: function(name, cb) {
+        orm.createOne("burgers", [
+            "burger_name", "devoured"
+        ], [
+            name, false
+        ], cb);
+    },
+    updateOne: function(id, cb) {
+        var condition = "id=" + id;
+        orm.updateOne("burgers", {
+            devoured: true
+        }, condition, cb);
     }
 };
+
+module.exports = burger;
